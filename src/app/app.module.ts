6076741f64b7df8b81e40227518bb4dd4e86ef6e
Hirendra <hirendra2008@gmail.com>
2019-02-17 14:44:40 +0530

    k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { BookComponent } from './book/book.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { BookCreateComponent } from './book-create/book-create.component';
-import { BookEditComponent } from './book-edit/book-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
@@ -37,21 +34,6 @@ const appRoutes: Routes = [
     path: 'books',
     component: BookComponent,
     data: { title: 'Book List' }
-  },
-  {
-    path: 'book-details/:id',
-    component: BookDetailComponent,
-    data: { title: 'Book Details' }
-  },
-  {
-    path: 'book-create',
-    component: BookCreateComponent,
-    data: { title: 'Create Book' }
-  },
-  {
-    path: 'book-edit/:id',
-    component: BookEditComponent,
-    data: { title: 'Edit Book' }
   }
   
 ];
@@ -60,9 +42,6 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     BookComponent,
-    BookDetailComponent,
-    BookCreateComponent,
-    BookEditComponent,
     LoginComponent,
     SearchComponent,
     RegisterComponent,
